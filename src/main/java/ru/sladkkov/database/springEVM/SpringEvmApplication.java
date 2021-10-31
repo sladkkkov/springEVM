aa94a3e6d861fcd04af57b6cf02924453ad69ff2
Danila <sladkkov@yandex.ru>
2021-11-01 00:22:19 +0400

    add css style

--- src/main/java/ru/sladkkov/database/springEVM/SpringEvmApplication.java
+++ src/main/java/ru/sladkkov/database/springEVM/SpringEvmApplication.java
@@ -9,4 +9,5 @@ public class SpringEvmApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringEvmApplication.class, args);
 	}
+
 }
--- src/main/java/ru/sladkkov/database/springEVM/repository/BuyerRepository.java
+++ src/main/java/ru/sladkkov/database/springEVM/repository/BuyerRepository.java
@@ -4,4 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import ru.sladkkov.database.springEVM.model.Buyer;
 
 public interface BuyerRepository extends JpaRepository<Buyer, Integer> {
+
+
 }
